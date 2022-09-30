ff6d4bd6b8f62e342c12383bbaf4da6ba347ea73
khanhtung97 <nguyenkhanhtung580@gmail.com>
2022-09-30 11:24:16 +0700

    add UseCors

--- Program.cs
+++ Program.cs
@@ -11,6 +11,9 @@ builder.Services.AddAuthenticationConfig(builder.Configuration);
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+// add Swapger config with jwt
+builder.Services.AddSwaggerGenConfiguration();
+
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
@@ -25,7 +28,13 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseAuthentication();
 app.UseAuthorization();
-
+app.UseCors(builder =>
+{
+    builder
+    .AllowAnyOrigin()
+    .AllowAnyHeader()
+    .AllowAnyMethod();
+});
 app.MapControllers();
 
 app.Run();
